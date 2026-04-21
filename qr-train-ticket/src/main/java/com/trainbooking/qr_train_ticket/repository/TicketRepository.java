d867c14cb5f033370c0ca150199123cf06ce221d
afeez-07 <afeez8288@gmail.com>
2026-04-21 21:55:20 +0530

    Delete qr-train-ticket directory

--- qr-train-ticket/src/main/java/com/trainbooking/qr_train_ticket/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.trainbooking.qr_train_ticket.repository;
-
-import com.trainbooking.qr_train_ticket.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket,Long> {
-}
